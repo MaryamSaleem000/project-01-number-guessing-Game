ff3d8f00dea433c19ee90045454f25e5fee385d6
Maryam Saleem <161303403+MaryamSaleem000@users.noreply.github.com>
2024-08-03 06:44:22 -0700

    Add files via upload

---
+++ number guess game project no1/app.ts
@@ -0,0 +1,37 @@
+import inquirer from 'inquirer';
+
+async function main() {
+  const maxNumber = 50;
+  const secretNumber = Math.floor(Math.random() * maxNumber) + 1;
+  let guessedCorrectly = false;
+
+  console.log(`Guess a number between 1 and ${maxNumber}`);
+
+  while (!guessedCorrectly) {
+    const { guess } = await inquirer.prompt({
+      type: 'input',
+      name: 'guess',
+      message: 'Enter your guess:',
+      validate: (input: string) => {
+        const number = Number(input);
+        if (isNaN(number) || number < 1 || number > maxNumber) {
+          return `Please enter a number between 1 and ${maxNumber}.`;
+        }
+        return true;
+      },
+    });
+
+    const userGuess = Number(guess);
+
+    if (userGuess < secretNumber) {
+      console.log('Too low! Try again.');
+    } else if (userGuess > secretNumber) {
+      console.log('Too high! Try again.');
+    } else {
+      console.log('Congratulations! You guessed the number.');
+      guessedCorrectly = true;
+    }
+  }
+}
+
+main().catch(err => console.error(err));
